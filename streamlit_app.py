c84eb59c2e0d80b520ddb063d8039fcffd0b8893
hiermon723 <121366113+hiermon723@users.noreply.github.com>
2022-12-25 17:33:57 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,5 @@
 import streamlit
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 Blueberry Oatmeal')
+st.text('Kale, Spins\ach and Rocket Smoothie')
