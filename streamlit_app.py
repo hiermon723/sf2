719f5e785d854771da93ab43596cde55da213f59
hiermon723 <121366113+hiermon723@users.noreply.github.com>
2022-12-25 17:54:09 -0500

    Update streamlit_app.py
    Added pandas

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,3 +6,4 @@ streamlit.text('🥗 Kale, Spinach and Rocket Smoothie')
 streamlit.text('🐔 Hard boiled, free-range egg')
 streamlit.text('🥑🍞 Avocado Toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+import pandas
